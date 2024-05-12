f5cc150691b088db72c899d53d4d7fe5989e6e5f
Max Shen <maxwshen@gmail.com>
2024-05-12 13:19:01 -0700

    setuptools build

--- README.md
+++ README.md
@@ -11,3 +11,52 @@ from hackerargs import args
 value = args.setdefault(key, default_value)
 
 ```
+
+Maintains a global write-once dict args, accessible from other scripts.
+
+Usage
+-----
+In a python script, import args.
+>>> from hackerargs import args
+
+Steps
+
+1 (Optional): Init default args from yaml. Doing this instantiates
+    arguments in dict with automatically inferred types. 
+    A default yaml gives the user an idea of the most important 
+    expected arguments.
+>>> args.init_from_yaml('args.yaml')
+
+# In main script, once:
+
+2. Parse args from CLI in --arg value format. 
+    If args were init'd from yaml, then types are interpreted accordingly. 
+    Unknown args, or when args were not init'd, infer types using
+    int > float > bool > str.
+    Parsing CLI args supports wandb sweeps.
+>>> args.parse_cli_args()
+
+3. In other scripts, access using setdefault method.
+    If key is not in args, this sets args[key] = value.
+>>> parameter_value = args.setdefault(key, value)
+
+4. After all code has run
+>>> args.save_to_yaml(output_yaml_file)
+
+Design philosophy
+-----------------
+Handling args as a global variable has these benefits:
++ Code readability: function signatures are concise, enabling understanding
+    at a higher level of abstraction
++ Easier development: To add a new argument to a function, only 1 line of
+    code change is needed: just add setdefault into the function. 
+    This system enables specifying values with CLI arguments without any 
+    other code changes. In contrast, explicitly changing the function
+    signature requires further code changes everywhere that function is called
+    to support calling the function with custom values.
+
+Only using setdefault guarantees that args entries are never updated
+twice, which lets us save args to a yaml file at the end of usage
+while achieving exact reproducibility: loading that yaml file loads
+the original argument values, if the default values in code changes over
+time.
--- hackerargs/args.py
+++ hackerargs/args.py
@@ -1,54 +1,3 @@
-"""
-    Maintains a global write-once dict args, accessible from other scripts.
-
-    Usage
-    -----
-    In a python script, import args.
-    >>> from hackerargs import args
-
-    Steps
-
-    1 (Optional): Init default args from yaml. Doing this instantiates
-        arguments in dict with automatically inferred types. 
-        A default yaml gives the user an idea of the most important 
-        expected arguments.
-    >>> args.init_from_yaml('args.yaml')
-    
-    # In main script, once:
-
-    2. Parse args from CLI in --arg value format. 
-        If args were init'd from yaml, then types are interpreted accordingly. 
-        Unknown args, or when args were not init'd, infer types using
-        int > float > bool > str.
-        Parsing CLI args supports wandb sweeps.
-    >>> args.parse_cli_args()
-
-    3. In other scripts, access using setdefault method.
-        If key is not in args, this sets args[key] = value.
-    >>> parameter_value = args.setdefault(key, value)
-
-    4. After all code has run
-    >>> args.save_to_yaml(output_yaml_file)
-    
-    Design philosophy
-    -----------------
-    Handling args as a global variable has these benefits:
-    + Code readability: function signatures are concise, enabling understanding
-      at a higher level of abstraction
-    + Easier development: To add a new argument to a function, only 1 line of
-      code change is needed: just add setdefault into the function. 
-      This system enables specifying values with CLI arguments without any 
-      other code changes. In contrast, explicitly changing the function
-      signature requires further code changes everywhere that function is called
-      to support calling the function with custom values.
-    
-    Only using setdefault guarantees that args entries are never updated
-    twice, which lets us save args to a yaml file at the end of usage
-    while achieving exact reproducibility: loading that yaml file loads
-    the original argument values, if the default values in code changes over
-    time.
-"""
-
 import sys
 import os
 from pathlib import Path
@@ -110,9 +59,6 @@ class WriteOnceDict:
     def __delitem__(self, key: str) -> None:
         raise KeyError('Cannot delete items in write-once only dict')
 
-    """
-        Access
-    """
     def get(self, key: str) -> Any:
         return self._privatedict[key]
 
@@ -131,9 +77,6 @@ class WriteOnceDict:
     def items(self):
         return self._privatedict.items()
 
-    """
-        Parsing
-    """
     def parse_args(self, *inps: list[str]) -> None:
         """ Parse args from sys.argv, and optional inputs
             config_yaml, ArgumentParser.
